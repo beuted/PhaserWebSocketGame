8427b3f935208668dd7ff8a0f60f0d1b5153195b
Benoit Jehanno <b.jehanno@criteo.com>
2016-01-12 12:22:24 +0100

    Adding watches and live reload (not tested yet)

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,13 +14,13 @@ module.exports = function(grunt) {
                 options: {
                     module: 'amd'
                 },
-                src: ["public/**/*.ts", "!**/*.d.ts"],
+                src: ["public/**/*.ts", "!**/*.d.ts"]
             },
             server : {
                 options: {
                     module: 'commonjs'
                 },
-                src: ["app.ts", "server/**/*.ts","!**/*.d.ts"],
+                src: ["app.ts", "server/**/*.ts","!**/*.d.ts"]
             }
         },
 
@@ -28,19 +28,62 @@ module.exports = function(grunt) {
             public: {
                 options: {
                     command: 'reinstall',
-                    config: './public/tsd.json',
+                    config: './public/tsd.json'
                 }
             },
             server: {
                 options: {
                     command: 'reinstall',
-                    config: './server/tsd.json',
+                    config: './server/tsd.json'
+                }
+            }
+        },
+
+        nodemon: {
+            server: {
+                src: ['app.js']
+            },
+            options: {
+                watch: ['server'],
+                env: {
+                    PORT: '8181'
+                },
+                delay: 2000,
+            }
+        },
+
+        watch: {
+            tsPublic: {
+                files: './public/**/*.ts',
+                tasks: ['ts:public'],
+                options: {
+                    atBegin: true,
+                    livereload: true
+                }
+            },
+            tsServer: {
+                files: ['./server/**/*.ts', './app.ts'],
+                tasks: ['ts:server'],
+                options: {
+                    atBegin: true
+                }
+            }
+        },
+
+        concurrent: {
+            watchers: {
+                tasks: ['nodemon:server', 'watch:tsServer', 'watch:tsPublic'],
+                options: {
+                    logConcurrentOutput: true
                 }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("grunt-concurrent");
+    grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-ts');
     grunt.loadNpmTasks('grunt-tsd');
 
